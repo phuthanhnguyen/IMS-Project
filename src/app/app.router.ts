9f6d497914d28eb597bf9bcaa1a76c1adc4e17e1
NGUYEN PhuThanh <nguyenphuthanh@NGUYENs-MacBook-Pro.local>
2016-11-06 12:44:07 +0100

    first commit

---
+++ src/app/app.router.ts
@@ -0,0 +1,12 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+export const router: Routes = [
+	{path: '', component: AppComponent},
+	{path: 'login', component: LoginComponent}
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
